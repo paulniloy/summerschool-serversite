88ef8cf3bc090e7ba1d14a901d01c212ac9157ee
paulniloy <110883440+paulniloy@users.noreply.github.com>
2023-06-13 16:28:00 +0600

    link correctlly working

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("summerschool");
     const classes = db.collection("popularclasses");
